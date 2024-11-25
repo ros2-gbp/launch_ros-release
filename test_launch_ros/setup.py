bed682c50836aeef7adf47287a821d01bea1e86f
Audrow Nash <audrow@intrinsic.ai>
2024-11-25 11:40:49 -0600

    Rebase from 'upstream'

--- test_launch_ros/setup.py
+++ test_launch_ros/setup.py
@@ -5,7 +5,7 @@ package_name = 'test_launch_ros'
 
 setup(
     name=package_name,
-    version='0.19.7',
+    version='0.19.8',
     packages=find_packages(exclude=['test']),
     install_requires=[
         'setuptools',
@@ -23,8 +23,8 @@ setup(
     author_email='william@osrfoundation.org',
     maintainer='Aditya Pande, Jacob Perron, Michel Hidalgo',
     maintainer_email='aditya.pande@openrobotics.org, jacob@openrobotics.org, michel@ekumenlabs.com',  # noqa: E501
-    url='https://github.com/ros2/launch',
-    download_url='https://github.com/ros2/launch/releases',
+    url='https://github.com/ros2/launch_ros',
+    download_url='https://github.com/ros2/launch_ros/releases',
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
